4cb8227ebbfc3dc7900b37c8a50fc822a8aa8eb5
tayyab <tayyab536@gmail.com>
2025-09-15 12:22:45 +0500

    app config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-import {connectDB}  from "./DB/db.js";
+import { connectDB } from "./DB/db.js";
 import dotenv from "dotenv";
+import { app } from "./app.js";
 dotenv.config({ path: "./env" });
 
-
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || "4000", () => {
+      console.log("app is working.....");
+    });
+  })
+  .catch((e) => {
+    throw e;
+  });
