9322ce41864a2fecb4ad40a32a966e04a7503d4f
tayyab <tayyab536@gmail.com>
2025-09-17 11:16:06 +0500

    intial setup of controller and users and router

---
+++ src/Controllers/user.controller.js
@@ -0,0 +1,9 @@
+const registerUser = (req, res) => {
+  try {
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
+export { registerUser };
--- src/app.js
+++ src/app.js
@@ -9,9 +9,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+////import route
+import userRouter from "./Routers/user.Router.js";
 
-
-
+app.use("/api/v1/user", userRouter);
 
 export { app };
-
