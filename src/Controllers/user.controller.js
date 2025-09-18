60e23cb818626a00d8763101dbc76ac75b6a3c89
tayyab <tayyab536@gmail.com>
2025-09-18 15:53:48 +0500

    Login Route

--- src/Controllers/user.controller.js
+++ src/Controllers/user.controller.js
@@ -1,6 +1,20 @@
 import { fileUploadCloudinary } from "../Utils/cloudnaryFIleUpload.js";
 import { User } from "../Models/User.Model.js";
 let uploaded = "";
+
+const genrateAccessTokenAndRefereshToken = async (userId) => {
+  try {
+    const alreadyUser = await User.findById(userId);
+    const accessToken = alreadyUser.genrateAccessToken();
+    const refreshToken = alreadyUser.genrateRefreshToken();
+    await alreadyUser.save({ validateBeforeSave: false });
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw error;
+  }
+};
+
 const registerUser = async (req, res) => {
   try {
     const { userName, lastName, email, password } = req.body;
@@ -18,11 +32,7 @@ const registerUser = async (req, res) => {
     }
     const avatar = req.files?.avatar[0].path;
     if (avatar) {
-      console.log("avatar");
-      console.log(avatar);
       uploaded = await fileUploadCloudinary(avatar);
-      console.log("uploaded");
-      console.log(uploaded);
     }
 
     const alreadyUser = await User.findOne({
@@ -52,4 +62,47 @@ const registerUser = async (req, res) => {
   }
 };
 
-export { registerUser };
+const loginUser = async (req, res) => {
+  // req body sa data len ha
+  //data check krna ha k ha k ni
+  //user check krna ha
+  /// password check
+  //access token or refreshtoken genrate token genrate krna ha
+  //cookies ma  store krna ha
+
+  const { email, userName, password } = req.body;
+  if (!(email || userName)) {
+    res.status(500).json({ message: "user Name or email is Required!" });
+    return;
+  }
+
+  const user = User.findOne({ $or: [{ email }, { userName }] });
+  if (!user) {
+    res.status(500).json({ message: "user is not exist" });
+    return;
+  }
+  const passwordCheck = await user.commparePassword(password);
+  if (!passwordCheck) {
+    res.status(500).json({ message: "password is not correct" });
+    return;
+  }
+
+  const { accessToken, refreshToken } = genrateAccessTokenAndRefereshToken(
+    user._id
+  );
+
+  const loggedInUser = User.findById(user._id).select("-password");
+
+  const options = {
+    httpOnly: true,
+    secure: ture,
+  };
+
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json({ message: "user is logged in " });
+};
+
+export { registerUser, loginUser };
